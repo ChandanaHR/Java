710775f3eb8dbbd545c3b5026e74a795947050ce
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2023-07-25 00:29:53 -0700

    Create linkedlist9.java

---
+++ linkedlist9.java
@@ -0,0 +1,110 @@
+Given a singly linked list, the task is to rearrange it in a way that all odd position nodes are together and all even positions node are together.
+Assume the first element to be at position 1 followed by second element at position 2 and so on.
+Note: You should place all odd positioned nodes first and then the even positioned ones. (considering 1 based indexing). Also, the relative order of odd positioned nodes and even positioned nodes should be maintained.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4
+Output: 1 3 2 4 
+Explanation: 
+Odd elements are 1, 3 and even elements are 
+2, 4. Hence, resultant linked list is 
+1->3->2->4.
+Example 2:
+
+Input:
+LinkedList: 1->2->3->4->5
+Output: 1 3 5 2 4 
+Explanation: 
+Odd elements are 1, 3, 5 and even elements are
+2, 4. Hence, resultant linked list is
+1->3->5->2->4.
+
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            new Solution().rearrangeEvenOdd(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+This is method only submission.
+You only need to complete the below method.
+*/
+class Solution
+{
+
+   // Should rearrange given linked list such that all even
+   // positioned Nodes are before odd positioned.
+   // Returns nothing
+    void rearrangeEvenOdd(Node head)
+    {
+          //  The task is to complete this method
+          if(head==null)
+          {
+              return;
+          }
+          Node odd = head;
+          Node even = head.next;
+          Node evenhead = even;
+          while(even!=null && even.next!=null)
+          {
+              odd.next = even.next;
+              odd = odd.next;
+              even.next = odd.next;
+              even = even.next;
+          }
+          odd.next = evenhead;
+     }
+}
