db90cfa96d4f4962ba4176e8ee69db714d2ecad5
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2023-08-04 02:30:15 -0700

    Create linkedlist12.java

---
+++ linkedlist12.java
@@ -0,0 +1,108 @@
+Given a linked list sorted in ascending order and an integer called data, insert data in the linked list such that the list remains sorted.
+
+Example 1:
+
+Input:
+LinkedList: 25->36->47->58->69->80
+data: 19
+Output: 19 25 36 47 58 69 80
+Example 2:
+
+Input:
+LinkedList: 50->100
+data: 75
+Output: 50 75 100
+
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head1, int key) {
+        // Add your code here.
+        Node item = new Node(key);
+        if(key<head1.data)
+        {
+            item.next = head1;
+            head1 = item;
+        }
+        else
+        {
+            Node prev = head1;
+            Node temp = head1;
+            while(temp!=null)
+            {
+                if(key<temp.data)
+                {
+                    prev.next = item;
+                    item.next = temp;
+                    break;
+                }
+                prev = temp;
+                temp = temp.next;
+            }
+            prev.next = item;
+        }
+        return head1;
+    }
+}
