6e8a4d00028e3c420643e33b4758f55e6e3171c1
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2025-06-15 12:22:24 +0530

    Update Accessmodifiers1.java

--- Accessmodifiers1.java
+++ Accessmodifiers1.java
@@ -56,3 +56,59 @@ public class Project1 {
 
 }
 //Here all the outputs are displayed because inside project1 we are having inner classes A and B which is valid
+
+
+package package1;
+
+public class Project1 {
+	public void publicmethod() {
+		System.out.println("This is a public method");
+	}
+	
+	private void privatemethod() {
+		System.out.println("This is a private method");
+	}
+	
+	protected void protectedmethod() {
+		System.out.println("This is a protected method");
+	}
+	
+	void defaultmethod() {
+		System.out.println("This is a default method");
+	}
+	
+	public class B extends Project1 {
+		public void publicmethod() {
+			System.out.println("This is a public method");
+		}
+		
+		private void privatemethod() {
+			System.out.println("This is a private method");
+		}
+		
+		protected void protectedmethod() {
+			System.out.println("This is a protected method");
+		}
+		
+		void defaultmethod() {
+			System.out.println("This is a default method");
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Project1 outer = new Project1();
+		outer.publicmethod();
+		outer.privatemethod();
+		outer.protectedmethod();
+		outer.defaultmethod();
+		
+		B object2 = outer.new B();
+		object2.publicmethod();
+		object2.privatemethod();
+		object2.protectedmethod();
+		object2.defaultmethod();
+	}
+
+}
+
