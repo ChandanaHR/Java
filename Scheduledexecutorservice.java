e64c7434706ba6a6bc380185c60b58cdccd1bea4
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-06-18 07:58:05 +0530

    Update Scheduledexecutorservice.java

--- Scheduledexecutorservice.java
+++ Scheduledexecutorservice.java
@@ -37,3 +37,93 @@ Methods
                     }, 5, TimeUnit.SECONDS);
                 }
             }
+Program Starts
+      |
+Wait 5 Seconds
+      |
+Execute Task
+      |
+Print Message
+
+  2) scheduleAtFixedRate() : Runs a task repeatedly at fixed intervals.
+    Syntax: scheduleAtFixedRate(
+    Runnable task,
+    long initialDelay,
+    long period,
+    TimeUnit unit
+)
+
+          import java.util.concurrent.*;
+        
+        public class FixedRateDemo {
+        
+            public static void main(String[] args) {
+        
+                ScheduledExecutorService service =
+                        Executors.newScheduledThreadPool(2);
+        
+                service.scheduleAtFixedRate(() -> {
+        
+                    System.out.println("Task Running");
+        
+                }, 2, 3, TimeUnit.SECONDS);
+            }
+        }
+2 Initial delay : Wait 2 seconds before first execution.
+  3 Period: Execute every 3 seconds.
+  Execution Timeline
+  0 sec -> Program Start
+
+2 sec -> Task Runs
+
+5 sec -> Task Runs
+
+8 sec -> Task Runs
+
+11 sec -> Task Runs
+
+14 sec -> Task Runs
+
+
+  3) scheduleWithFixedDelay() : Runs tasks repeatedly but waits a fixed delay after the previous execution completes.
+  scheduleWithFixedDelay(
+    Runnable task,
+    long initialDelay,
+    long delay,
+    TimeUnit unit
+)
+
+          import java.util.concurrent.*;
+        
+        public class FixedDelayDemo {
+        
+            public static void main(String[] args) {
+        
+                ScheduledExecutorService service =
+                        Executors.newScheduledThreadPool(2);
+        
+                service.scheduleWithFixedDelay(() -> {
+        
+                    System.out.println("Task Running");
+        
+                    try {
+                        Thread.sleep(2000);
+                    }
+                    catch(Exception e) {
+        
+                    }
+        
+                }, 2, 3, TimeUnit.SECONDS);
+            }
+        }
+0 sec  Program Start
+
+2 sec  Task Starts
+
+4 sec  Task Ends
+
+7 sec  Task Starts
+
+9 sec  Task Ends
+
+12 sec Task Starts
